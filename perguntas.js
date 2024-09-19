db9a0d84755d94e1e66a8067bd4855d7da0c08d5
Mariane <138937172+20mariane07@users.noreply.github.com>
2024-09-19 14:18:42 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,47 @@
 criaCartao(
-    'Programação',
+    'Histórias',
+    'Em que dia eu nasci?',
+    'Nasci dia:31/08/2007'
+)
+
+criaCartao(
+    'Histórias',
+    'Qual é minha cor preferida?',
+    'Minha cor preferida é preto'
+)
+
+criaCartao(
+    'Histórias',
+    'Quais são minha comida favorita?',
+    'Minha comida favorita é lazanha'
+)
+
+criaCartao(
+    'Histórias',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Histórias',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'Geografia',
+    'Histórias',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
+    'Histórias',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Histórias',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
